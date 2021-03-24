b0cef39e6cd1633ebbc7d5947f67a8c61812e1f4
GizemErgin <gizemergin93@gmail.com>
2021-03-24 19:11:37 +0300

    20.03.21 - Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from  "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +15,7 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import{ToastrModule} from "ngx-toastr";
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 
 @NgModule({
   declarations: [
@@ -24,13 +25,15 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     NaviComponent,
     VatAddedPipe,
     FilterPipePipe,
-    CartSummaryComponent
+    CartSummaryComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right"
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 
 @Injectable({
@@ -21,5 +22,10 @@ export class ProductService {
   getProductsByCategory(categoryId: number): Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + 'products/getbycategory?categoryId=' + categoryId;
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"products/add"
+    return this.httpClient.post<ResponseModel>(newPath, product)
   }
 }
